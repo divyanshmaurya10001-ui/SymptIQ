222cf2d681fa5b97c234d005c35bc1f9a8f61aca
Divyansh Maurya <tumhara@email.com>
2026-06-22 12:11:40 +0530

    Day 5 - ML model connected, disease prediction working

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+import pickle
+import numpy as np
 
 app = Flask(__name__)
+
+# Load model
+with open('models/model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/')
 def home():
@@ -8,8 +14,19 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    symptoms = request.form['symptoms']
-    return render_template('result.html', symptoms=symptoms)
+    fever = 1 if request.form.get('fever') == 'yes' else 0
+    cough = 1 if request.form.get('cough') == 'yes' else 0
+    fatigue = 1 if request.form.get('fatigue') == 'yes' else 0
+    breathing = 1 if request.form.get('breathing') == 'yes' else 0
+    age = int(request.form.get('age', 25))
+    gender = 1 if request.form.get('gender') == 'male' else 0
+    bp = int(request.form.get('bp', 1))
+    cholesterol = int(request.form.get('cholesterol', 1))
+
+    features = np.array([[fever, cough, fatigue, breathing, age, gender, bp, cholesterol]])
+    prediction = model.predict(features)[0]
+
+    return render_template('result.html', disease=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
